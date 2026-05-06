cc8cd8e1a72b237d807474b4a16ecbeb1e73801f
gopikrishnachapparam <gopikrishnachapparam@gmail.com>
2026-05-06 17:11:31 +0530

    Add files via upload
    day2

---
+++ html-css/activity.js
@@ -0,0 +1,4 @@
+function showProfile() {
+    document.getElementById("welcome").style.display = "none";
+    document.getElementById("profile").style.display = "block";
+}
---
+++ html-css/day14activity2.js
@@ -0,0 +1,11 @@
+function showProfile() {
+    document.getElementById("welcome").style.display = "none";
+    document.getElementById("timetable").style.display = "none";
+    document.getElementById("profile").style.display = "block";
+}
+
+function showTimeTable() {
+    document.getElementById("welcome").style.display = "none";
+    document.getElementById("profile").style.display = "none";
+    document.getElementById("timetable").style.display = "block";
+}
